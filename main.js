8159642ceb5f58e0c44407e15af94ffd4fb76d8e
veerasiva <veerasiva984@gmail.com>
2025-02-14 13:17:34 +0530

    route structure

--- main.js
+++ main.js
@@ -7,6 +7,19 @@ app.get("/", (req, res) => {
   res.json({ msg: "Hello student" });
 });
 
+//create CURD funcionality for movies
+
+//R-for reading movies
+app.get("/movies", () => {});
+
+//c-for creating movies
+app.post("/movies", () => {});
+
+//U -for updating movies
+app.put("/movies/:id", () => {});
+//D for Deliting movies
+app.delete("/movies/:id", () => {});
+
 app.listen(PORT, () => {
   console.log(`The server is running at http://localhost:${PORT}`);
 });
