c64505b63393b87f01bad1a28b8ccc3b002cb5fb
veerasiva <veerasiva984@gmail.com>
2025-02-14 16:25:14 +0530

    connect to mongoDB with env

--- READE.md
+++ READE.md
@@ -4,3 +4,7 @@ provide a movie api for react.js app
 ###Important Comment
 
 1.`npm install` 2.`npm run server` 3. `git init`4.`git add .` 5.`git commit -m "Your msg"` 6.`git push -u origin master`
+
+###Data Base
+void0error
+VFEwGT1H6FJFp0Yv
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import express from "express";
 import movieRoutes from "./routes/movies.route.js";
+import connectDB from "./lib/db.js";
 
 const app = express();
 const PORT = 6969;
@@ -7,6 +8,10 @@ const PORT = 6969;
 app.get("/", (req, res) => {
   res.json({ msg: "Hello student" });
 });
+
+//connectionDB
+
+connectDB();
 
 app.use("/movies", movieRoutes);
 
